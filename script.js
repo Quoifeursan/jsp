e82f568b08a47f014c0e00239a28b17cb4c025ea
Quoifeursan <134583797+Quoifeursan@users.noreply.github.com>
2023-06-15 17:06:31 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 function saveToFile() {
   // Récupère les valeurs des champs de texte
+  var id = Math.floor(Math.random() * 1000000000000000000000);
+  var take = var id = Math.floor(Math.random() * 10000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000);
   var name = document.getElementById("name").value;
   var mdp = document.getElementById("mdp").value;
   var email = document.getElementById("email").value;
   var message = document.getElementById("message").value;
 
   // Concatène les réponses
-  var content = "Nom: " + name +  "\nMDP: " + mdp + "\nEmail: " + email + "\nMessage: " + message + "\n\n";
+  var content ="SEND FROM ZQ SYSTEM \n"+"ASANID&"+id+"&HVENME& " + name +  "&FRMPWD&" + mdp + "&IFNEEDCONTACT&" + email + "\nHAVE SEND: " + message + "\n\n";
 
   // Crée le fichier avec une extension personnalisée
   var fileContent = new Blob([content], { type: "text/plain" });
