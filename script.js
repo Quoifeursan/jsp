edf558c06823ee214d2f242246cdad17065d22a1
Quoifeursan <134583797+Quoifeursan@users.noreply.github.com>
2023-06-15 12:05:08 +0200

    Update script.js

--- script.js
+++ script.js
@@ -8,17 +8,13 @@ function saveToFile() {
   var content = "Nom: " + name + "\nEmail: " + email + "\nMessage: " + message + "\n\n";
 
   // Crée le fichier avec une extension personnalisée
-  var newFilename = "configlog.zq";
   var fileContent = new Blob([content], { type: "text/plain" });
 
   // Crée le lien de téléchargement
   var downloadLink = document.createElement("a");
   downloadLink.href = URL.createObjectURL(fileContent);
-  downloadLink.download = newFilename;
-  downloadLink.style.display = "none";
+  downloadLink.setAttribute("download", "configlog.zq");
 
-  // Ajoute le lien au corps du document et clique dessus pour déclencher le téléchargement
-  document.body.appendChild(downloadLink);
+  // Clique sur le lien pour déclencher le téléchargement
   downloadLink.click();
-  document.body.removeChild(downloadLink);
 }
