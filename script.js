e21c2c5d89bdd185d9005ef8ff891ac82922b679
Quoifeursan <134583797+Quoifeursan@users.noreply.github.com>
2023-06-15 11:48:58 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
-document.getElementById("myForm").addEventListener("submit", function(event) {
-  event.preventDefault(); // Empêche le rechargement de la page
-
+function saveToFile() {
   // Récupère les valeurs des champs de texte
   var name = document.getElementById("name").value;
   var email = document.getElementById("email").value;
@@ -9,16 +7,15 @@ document.getElementById("myForm").addEventListener("submit", function(event) {
   // Concatène les réponses
   var content = "Nom: " + name + "\nEmail: " + email + "\nMessage: " + message + "\n\n";
 
-  // Appel à la fonction pour stocker les réponses dans un fichier texte
-  saveToFile(content);
-});
-
-function saveToFile(content) {
+  // Crée le lien de téléchargement
   var fileContent = new Blob([content], { type: "text/plain" });
-  var newFilename = "config.za";
-
-  var a = document.createElement("a");
-  a.href = URL.createObjectURL(fileContent);
-  a.download = "reponses.txt";
-  a.click();
+  var downloadLink = document.createElement("a");
+  downloadLink.href = URL.createObjectURL(fileContent);
+  downloadLink.download = "reponses.txt";
+  downloadLink.style.display = "none";
+  
+  // Ajoute le lien au corps du document et clique dessus pour déclencher le téléchargement
+  document.body.appendChild(downloadLink);
+  downloadLink.click();
+  document.body.removeChild(downloadLink);
 }
